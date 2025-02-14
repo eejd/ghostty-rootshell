aswinmprabhu/ghostty

Add tab title rename feature to macos

macos/Sources/App/macOS/AppDelegate.swift
@@ -50,6 +50,7 @@ class AppDelegate: NSObject,
     @IBOutlet private var menuIncreaseFontSize: NSMenuItem?
     @IBOutlet private var menuDecreaseFontSize: NSMenuItem?
     @IBOutlet private var menuResetFontSize: NSMenuItem?
+    @IBOutlet private var menuSetTitle: NSMenuItem?
     @IBOutlet private var menuQuickTerminal: NSMenuItem?
     @IBOutlet private var menuTerminalInspector: NSMenuItem?
 
@@ -371,6 +372,7 @@ class AppDelegate: NSObject,
         syncMenuShortcut(config, action: "increase_font_size:1", menuItem: self.menuIncreaseFontSize)
         syncMenuShortcut(config, action: "decrease_font_size:1", menuItem: self.menuDecreaseFontSize)
         syncMenuShortcut(config, action: "reset_font_size", menuItem: self.menuResetFontSize)
+        syncMenuShortcut(config, action: "set_title", menuItem: self.menuSetTitle)
         syncMenuShortcut(config, action: "toggle_quick_terminal", menuItem: self.menuQuickTerminal)
         syncMenuShortcut(config, action: "toggle_visibility", menuItem: self.menuToggleVisibility)
         syncMenuShortcut(config, action: "inspector:toggle", menuItem: self.menuTerminalInspector)

macos/Sources/Ghostty/SurfaceView_AppKit.swift
@@ -14,6 +14,11 @@ extension Ghostty {
         // to the app level and it is set from there.
         @Published private(set) var title: String = "👻"
 
+        // This indicates if the title was set manually by the user. This is
+        // used to prevent the title from being changed automatically based
+        // on the running process.
+        @Published var titleSetManually: Bool = false
+
         // The current pwd of the surface as defined by the pty. This can be
         // changed with escape codes.
         @Published var pwd: String? = nil
@@ -342,7 +347,7 @@ extension Ghostty {
             NSCursor.setHiddenUntilMouseMoves(!visible)
         }
 
-        func setTitle(_ title: String) {
+        func setTitle(_ title: String, ask: Bool = false) {
             // This fixes an issue where very quick changes to the title could
             // cause an unpleasant flickering. We set a timer so that we can
             // coalesce rapid changes. The timer is short enough that it still
@@ -352,7 +357,51 @@ extension Ghostty {
                 withTimeInterval: 0.075,
                 repeats: false
             ) { [weak self] _ in
-                self?.title = title
+                // Create a dialog and ask the uesr if 
+                // the ask parameter is true
+                if ask {
+                    // Create an alert dialog
+                    let alert = NSAlert()
+                    alert.messageText = "Set Tab Title"
+                    alert.informativeText = "New title (leave empty to auto set):"
+                    alert.alertStyle = .informational
+                   
+                    // Add a text field to the alert
+                    let textField = NSTextField(frame: NSRect(x: 0, y: 0, width: 250, height: 24))
+                    textField.stringValue = self?.title ?? ""
+                    alert.accessoryView = textField
+                   
+                    // Add buttons
+                    alert.addButton(withTitle: "OK")
+                    alert.addButton(withTitle: "Cancel")
+                   
+                    let response = alert.runModal()
+                   
+                    // Check if the user clicked "OK"
+                    if response == .alertFirstButtonReturn {
+                        // Get the input text
+                        let newTitle = textField.stringValue
+                        
+                        if newTitle.isEmpty {
+                            // Empty means that user wants the title to be set automatically
+                            // We also need to reload the config for the "title" property to be
+                            // used again by this tab.
+                            self?.titleSetManually = false
+                            let action = "reload_config"
+                            if (!ghostty_surface_binding_action(self?.surface, action, UInt(action.count))) {
+                                logger.warning("action failed action=\(action)")
+                            }
+                        } else {
+                            // Set the title and prevent it from being changed automatically
+                            self?.title = newTitle
+                            self?.titleSetManually = true
+                        }
+                    }
+                }
+                if self?.titleSetManually == false {
+                    // Change the title if it wasn't set manually
+                    self?.title = title
+                }
             }
         }
 
@@ -1022,6 +1071,8 @@ extension Ghostty {
             menu.addItem(.separator())
             menu.addItem(withTitle: "Reset Terminal", action: #selector(resetTerminal(_:)), keyEquivalent: "")
             menu.addItem(withTitle: "Toggle Terminal Inspector", action: #selector(toggleTerminalInspector(_:)), keyEquivalent: "")
+            menu.addItem(.separator())
+            menu.addItem(withTitle: "Set Title", action: #selector(setTitle(_:)), keyEquivalent: "")
 
             return menu
         }
@@ -1085,6 +1136,10 @@ extension Ghostty {
             if (!ghostty_surface_binding_action(surface, action, UInt(action.count))) {
                 AppDelegate.logger.warning("action failed action=\(action)")
             }
+        }
+        
+        @IBAction func setTitle(_ sender: Any) {
+            setTitle("", ask: true)
         }
 
         /// Show a user notification and associate it with this surface

eejd/ghostty-rootshell

macos/Sources/App/macOS/AppDelegate.swift
@@ -52,6 +52,7 @@ class AppDelegate: NSObject,
     @IBOutlet private var menuIncreaseFontSize: NSMenuItem?
     @IBOutlet private var menuDecreaseFontSize: NSMenuItem?
     @IBOutlet private var menuResetFontSize: NSMenuItem?
+    @IBOutlet private var menuChangeTitle: NSMenuItem?
     @IBOutlet private var menuQuickTerminal: NSMenuItem?
     @IBOutlet private var menuTerminalInspector: NSMenuItem?
 
@@ -384,6 +385,7 @@ class AppDelegate: NSObject,
         syncMenuShortcut(config, action: "increase_font_size:1", menuItem: self.menuIncreaseFontSize)
         syncMenuShortcut(config, action: "decrease_font_size:1", menuItem: self.menuDecreaseFontSize)
         syncMenuShortcut(config, action: "reset_font_size", menuItem: self.menuResetFontSize)
+        syncMenuShortcut(config, action: "change_title_prompt", menuItem: self.menuChangeTitle)
         syncMenuShortcut(config, action: "toggle_quick_terminal", menuItem: self.menuQuickTerminal)
         syncMenuShortcut(config, action: "toggle_visibility", menuItem: self.menuToggleVisibility)
         syncMenuShortcut(config, action: "inspector:toggle", menuItem: self.menuTerminalInspector)

macos/Sources/Ghostty/SurfaceView_AppKit.swift
@@ -124,6 +124,11 @@ extension Ghostty {
         // A timer to fallback to ghost emoji if no title is set within the grace period
         private var titleFallbackTimer: Timer?
 
+        // This is the title from the terminal. This is nil if we're currently using
+        // the terminal title as the main title property. If the title is set manually
+        // by the user, this is set to the prior value (which may be empty, but non-nil).
+        private var titleFromTerminal: String?
+
         /// Event monitor (see individual events for why)
         private var eventMonitor: Any? = nil
 
@@ -380,6 +385,45 @@ extension Ghostty {
             NSCursor.setHiddenUntilMouseMoves(!visible)
         }
 
+        /// Set the title by prompting the user.
+        func promptTitle() {
+            // Create an alert dialog
+            let alert = NSAlert()
+            alert.messageText = "Change Terminal Title"
+            alert.informativeText = "Leave blank to restore the default."
+            alert.alertStyle = .informational
+
+            // Add a text field to the alert
+            let textField = NSTextField(frame: NSRect(x: 0, y: 0, width: 250, height: 24))
+            textField.stringValue = title
+            alert.accessoryView = textField
+
+            // Add buttons
+            alert.addButton(withTitle: "OK")
+            alert.addButton(withTitle: "Cancel")
+
+            let response = alert.runModal()
+
+            // Check if the user clicked "OK"
+            if response == .alertFirstButtonReturn {
+                // Get the input text
+                let newTitle = textField.stringValue
+
+                if newTitle.isEmpty {
+                    // Empty means that user wants the title to be set automatically
+                    // We also need to reload the config for the "title" property to be
+                    // used again by this tab.
+                    let prevTitle = titleFromTerminal ?? "👻"
+                    titleFromTerminal = nil
+                    setTitle(prevTitle)
+                } else {
+                    // Set the title and prevent it from being changed automatically
+                    titleFromTerminal = title
+                    title = newTitle
+                }
+            }
+        }
+
         func setTitle(_ title: String) {
             // This fixes an issue where very quick changes to the title could
             // cause an unpleasant flickering. We set a timer so that we can
@@ -390,6 +434,11 @@ extension Ghostty {
                 withTimeInterval: 0.075,
                 repeats: false
             ) { [weak self] _ in
+                // Set the title if it wasn't manually set.
+                guard self?.titleFromTerminal == nil else {
+                    self?.titleFromTerminal = title
+                    return
+                }
                 self?.title = title
             }
         }
@@ -1117,6 +1166,8 @@ extension Ghostty {
             menu.addItem(.separator())
             menu.addItem(withTitle: "Reset Terminal", action: #selector(resetTerminal(_:)), keyEquivalent: "")
             menu.addItem(withTitle: "Toggle Terminal Inspector", action: #selector(toggleTerminalInspector(_:)), keyEquivalent: "")
+            menu.addItem(.separator())
+            menu.addItem(withTitle: "Change Title...", action: #selector(changeTitle(_:)), keyEquivalent: "")
 
             return menu
         }
@@ -1188,6 +1239,10 @@ extension Ghostty {
             if (!ghostty_surface_binding_action(surface, action, UInt(action.count))) {
                 AppDelegate.logger.warning("action failed action=\(action)")
             }
+        }
+        
+        @IBAction func changeTitle(_ sender: Any) {
+            promptTitle()
         }
 
         /// Show a user notification and associate it with this surface