ghostty-org/ghostty

add Spanish (Latin America) locale support

src/os/i18n.zig
@@ -29,6 +29,7 @@ pub const locales = [_][:0]const u8{
     "nb_NO.UTF-8",
     "uk_UA.UTF-8",
     "pl_PL.UTF-8",
+    "es_419.UTF-8",
 };
 
 /// Set for faster membership lookup of locales.

eejd/ghostty-rootshell

src/os/i18n.zig
@@ -40,6 +40,7 @@ pub const locales = [_][:0]const u8{
     "pl_PL.UTF-8",
     "ja_JP.UTF-8",
     "id_ID.UTF-8",
+    "es_BO.UTF-8",
 };
 
 /// Set for faster membership lookup of locales.