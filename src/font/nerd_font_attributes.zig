ghostty-org/ghostty

Skip patchsets and codepoints not in SymbolsNF

src/font/nerd_font_attributes.zig
@@ -9,18 +9,6 @@ const Constraint = @import("face.zig").RenderOptions.Constraint;
 /// Get the constraints for the provided codepoint.
 pub fn getConstraint(cp: u21) ?Constraint {
     return switch (cp) {
-        0x2500...0x259f,
-        => .{
-            .size = .stretch,
-            .max_constraint_width = 1,
-            .align_horizontal = .center1,
-            .align_horizontal_grouped = false,
-            .align_vertical = .center1,
-            .pad_left = -0.02,
-            .pad_right = -0.02,
-            .pad_top = -0.01,
-            .pad_bottom = -0.01,
-        },
         0x2630,
         => .{
             .size = .cover,
@@ -319,8 +307,6 @@ pub fn getConstraint(cp: u21) ?Constraint {
             .align_vertical = .center1,
         },
         0xe0cf,
-        0xe0d3,
-        0xe0d5,
         => .{
             .size = .fit_cover1,
             .align_horizontal = .center1,
@@ -383,16 +369,24 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0x2665,
         0x26a1,
         0x2b58,
-        0xe000...0xe0a9,
-        0xe4fa...0xe7ef,
+        0xe000...0xe00a,
+        0xe0a0...0xe0a3,
+        0xe5fa...0xe6b8,
+        0xe700...0xe7ef,
         0xea60,
         0xea62...0xea7c,
-        0xea7e...0xea98,
+        0xea7e...0xea88,
+        0xea8a...0xea8c,
+        0xea8f...0xea98,
         0xeaa3...0xeab3,
-        0xeab8...0xead3,
-        0xead7...0xeb12,
+        0xeab8...0xeac7,
+        0xeac9,
+        0xeacc...0xead3,
+        0xead7...0xeb09,
+        0xeb0b...0xeb12,
         0xeb14...0xeb42,
-        0xeb44...0xeb6d,
+        0xeb44...0xeb4e,
+        0xeb50...0xeb6d,
         0xeb72...0xeb88,
         0xeb8c...0xeb99,
         0xeb9b...0xebca,
@@ -400,7 +394,8 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0xebd8...0xec06,
         0xec0a,
         0xec0d...0xec1e,
-        0xed00...0xf018,
+        0xed00...0xefce,
+        0xf000...0xf018,
         0xf01a...0xf02f,
         0xf031...0xf03c,
         0xf041...0xf043,
@@ -420,11 +415,19 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0xf0cb...0xf0d5,
         0xf0d7...0xf0dd,
         0xf0df...0xf0e6,
-        0xf0e8...0xf295,
+        0xf0e8...0xf105,
+        0xf108...0xf12f,
+        0xf132...0xf140,
+        0xf143...0xf152,
+        0xf15b...0xf174,
+        0xf179...0xf181,
+        0xf184...0xf220,
+        0xf22e...0xf254,
+        0xf25c...0xf295,
         0xf297...0xf2c3,
         0xf2c6...0xf2ef,
         0xf2f1...0xf305,
-        0xf307...0xf847,
+        0xf307...0xf381,
         0xf0001...0xf1af0,
         => .{
             .size = .fit_cover1,
@@ -608,19 +611,6 @@ pub fn getConstraint(cp: u21) ?Constraint {
             .align_vertical = .center1,
             .relative_height = 0.8780760626398211,
             .relative_y = 0.0687919463087248,
-        },
-        0xeb13,
-        0xeb89,
-        0xeb8b,
-        0xebcb,
-        0xebd7,
-        0xec08...0xec09,
-        => .{
-            .size = .fit_cover1,
-            .height = .icon,
-            .align_horizontal = .center1,
-            .align_horizontal_grouped = true,
-            .align_vertical = .center1,
         },
         0xeb43,
         => .{
@@ -1021,6 +1011,398 @@ pub fn getConstraint(cp: u21) ?Constraint {
             .relative_height = 0.9998803756692248,
             .relative_x = 0.0006021702214782,
             .relative_y = 0.0001196243307751,
+        },
+        0xf106...0xf107,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9238281250000000,
+            .relative_height = 0.5000000000000000,
+            .relative_y = 0.2853688029020556,
+        },
+        0xf130,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5500899185443775,
+            .relative_height = 0.9998602571268865,
+        },
+        0xf131,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9996826404316090,
+            .relative_x = 0.0003173595683910,
+        },
+        0xf141,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_height = 0.2593984962406015,
+            .relative_y = 0.3696741854636592,
+        },
+        0xf142,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.2589341692789969,
+        },
+        0xf153,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6245738204110751,
+            .relative_height = 0.8751322751322751,
+            .relative_x = 0.0003982176936827,
+            .relative_y = 0.0624338624338624,
+        },
+        0xf154,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6253613886028323,
+            .relative_height = 0.8751322751322751,
+            .relative_y = 0.0624338624338624,
+        },
+        0xf155,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5645794520717571,
+            .relative_x = 0.0010431817015224,
+        },
+        0xf156,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.8740995161332579,
+            .relative_height = 0.8752505446623093,
+            .relative_x = 0.0003982176936827,
+            .relative_y = 0.0623155929038282,
+        },
+        0xf157,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6244022504277511,
+            .relative_height = 0.8756468797564688,
+            .relative_x = 0.0009410987697650,
+            .relative_y = 0.0624338624338624,
+        },
+        0xf158,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.7492772227943355,
+            .relative_height = 0.8751322751322751,
+            .relative_x = 0.0003982176936827,
+            .relative_y = 0.0624338624338624,
+        },
+        0xf159,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9996446258939432,
+            .relative_height = 0.8756067947646895,
+            .relative_x = 0.0003553741060568,
+            .relative_y = 0.0623492063492063,
+        },
+        0xf15a,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.7343465808285431,
+            .relative_x = 0.0003982176936827,
+        },
+        0xf175,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6253968253968254,
+            .relative_height = 0.9989423585404548,
+            .relative_y = 0.0005288207297726,
+        },
+        0xf176,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6253968253968254,
+        },
+        0xf177...0xf178,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_height = 0.6250661025912215,
+            .relative_y = 0.1877313590692755,
+        },
+        0xf182,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = true,
+            .align_vertical = .center1,
+            .relative_width = 0.9998046921689268,
+        },
+        0xeb13,
+        0xeb89,
+        0xeb8b,
+        0xebcb,
+        0xebd7,
+        0xec08...0xec09,
+        0xf183,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = true,
+            .align_vertical = .center1,
+        },
+        0xf221,
+        0xf22c,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5501481168006771,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf222,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6999576809140923,
+            .relative_height = 0.8746819883943630,
+            .relative_y = 0.0624017379870223,
+        },
+        0xf223,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5501481168006771,
+        },
+        0xf224,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.6250528988573847,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf225,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.7998307236563690,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf226,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9500634786288616,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf227,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_height = 0.8746819883943630,
+            .relative_y = 0.0624017379870223,
+        },
+        0xf228,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf229,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.7498942022852306,
+            .relative_height = 0.9370837263813853,
+            .relative_y = 0.0624017379870223,
+        },
+        0xf22a,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5002115954295387,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf22b,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9250952179432924,
+            .relative_height = 0.6874767744332962,
+            .relative_y = 0.1560043449675557,
+        },
+        0xf22d,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.5501481168006771,
+            .relative_height = 0.6874767744332962,
+            .relative_y = 0.1560043449675557,
+        },
+        0xf255,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.7497700291899957,
+            .relative_height = 0.9993997599039616,
+        },
+        0xf256,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9369481605956786,
+            .relative_height = 0.9993997599039616,
+        },
+        0xf257,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9988801016527891,
+            .relative_height = 0.7810124049619848,
+            .relative_x = 0.0004607694114506,
+            .relative_y = 0.0935945806894186,
+        },
+        0xf258,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9993408710642397,
+            .relative_height = 0.7498142113988452,
+            .relative_y = 0.1247927742525582,
+        },
+        0xf259,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.9994712482163681,
+            .relative_x = 0.0005287517836319,
+        },
+        0xf25a,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.8746721401759192,
+            .relative_height = 0.9993997599039616,
+            .relative_x = 0.0004120617348304,
+        },
+        0xf25b,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_horizontal_grouped = false,
+            .align_vertical = .center1,
+            .relative_width = 0.8115482443424440,
+            .relative_height = 0.9975006099019084,
+            .relative_x = 0.0000857435324808,
         },
         0xf296,
         => .{

eejd/ghostty-rootshell

src/font/nerd_font_attributes.zig
@@ -9,17 +9,6 @@ const Constraint = @import("face.zig").RenderOptions.Constraint;
 /// Get the constraints for the provided codepoint.
 pub fn getConstraint(cp: u21) ?Constraint {
     return switch (cp) {
-        0x2500...0x259f,
-        => .{
-            .size = .stretch,
-            .max_constraint_width = 1,
-            .align_horizontal = .center1,
-            .align_vertical = .center1,
-            .pad_left = -0.02,
-            .pad_right = -0.02,
-            .pad_top = -0.01,
-            .pad_bottom = -0.01,
-        },
         0x2630,
         => .{
             .size = .cover,
@@ -291,8 +280,6 @@ pub fn getConstraint(cp: u21) ?Constraint {
             .align_vertical = .center1,
         },
         0xe0cf,
-        0xe0d3,
-        0xe0d5,
         => .{
             .size = .fit_cover1,
             .align_horizontal = .center1,
@@ -350,22 +337,31 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0x2665,
         0x26a1,
         0x2b58,
-        0xe000...0xe0a9,
-        0xe4fa...0xe7ef,
+        0xe000...0xe00a,
+        0xe0a0...0xe0a3,
+        0xe5fa...0xe6b8,
+        0xe700...0xe7ef,
         0xea60,
         0xea62...0xea7c,
-        0xea7e...0xea98,
+        0xea7e...0xea88,
+        0xea8a...0xea8c,
+        0xea8f...0xea98,
         0xeaa3...0xeab3,
-        0xeab8...0xead3,
-        0xead7...0xeb42,
-        0xeb44...0xeb6d,
+        0xeab8...0xeac7,
+        0xeac9,
+        0xeacc...0xead3,
+        0xead7...0xeb09,
+        0xeb0b...0xeb42,
+        0xeb44...0xeb4e,
+        0xeb50...0xeb6d,
         0xeb72...0xeb89,
         0xeb8b...0xeb99,
         0xeb9b...0xebd4,
         0xebd7...0xec06,
         0xec08...0xec0a,
         0xec0d...0xec1e,
-        0xed00...0xf02f,
+        0xed00...0xefce,
+        0xf000...0xf02f,
         0xf031...0xf03c,
         0xf03f,
         0xf041...0xf043,
@@ -384,10 +380,22 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0xf0cb...0xf0d5,
         0xf0d7...0xf0dd,
         0xf0df...0xf0e6,
-        0xf0e8...0xf295,
+        0xf0e8...0xf105,
+        0xf108...0xf12f,
+        0xf131...0xf140,
+        0xf142...0xf152,
+        0xf155,
+        0xf15a...0xf174,
+        0xf176,
+        0xf179...0xf181,
+        0xf183...0xf220,
+        0xf223,
+        0xf22e...0xf254,
+        0xf259,
+        0xf25c...0xf295,
         0xf297...0xf2c4,
         0xf2c6...0xf2ef,
-        0xf2f1...0xf847,
+        0xf2f1...0xf381,
         0xf0001...0xf1af0,
         => .{
             .size = .fit_cover1,
@@ -672,6 +680,8 @@ pub fn getConstraint(cp: u21) ?Constraint {
         0xf03e,
         0xf071,
         0xf08c,
+        0xf153...0xf154,
+        0xf158,
         => .{
             .size = .fit_cover1,
             .height = .icon,
@@ -827,6 +837,161 @@ pub fn getConstraint(cp: u21) ?Constraint {
             .align_vertical = .center1,
             .relative_height = 0.9998803756692248,
             .relative_y = 0.0001196243307751,
+        },
+        0xf106...0xf107,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.5000000000000000,
+            .relative_y = 0.2853688029020556,
+        },
+        0xf130,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9998602571268865,
+        },
+        0xf141,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.2593984962406015,
+            .relative_y = 0.3696741854636592,
+        },
+        0xf156,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.8752505446623093,
+            .relative_y = 0.0623155929038282,
+        },
+        0xf157,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.8756468797564688,
+            .relative_y = 0.0624338624338624,
+        },
+        0xf159,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.8756067947646895,
+            .relative_y = 0.0623492063492063,
+        },
+        0xf175,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9989423585404548,
+            .relative_y = 0.0005288207297726,
+        },
+        0xf177...0xf178,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.6250661025912215,
+            .relative_y = 0.1877313590692755,
+        },
+        0xf182,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_width = 0.9998046921689268,
+        },
+        0xf221,
+        0xf224...0xf226,
+        0xf228,
+        0xf22a,
+        0xf22c,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9994854643684076,
+        },
+        0xf222,
+        0xf227,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.8746819883943630,
+            .relative_y = 0.0624017379870223,
+        },
+        0xf229,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9370837263813853,
+            .relative_y = 0.0624017379870223,
+        },
+        0xf22b,
+        0xf22d,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.6874767744332962,
+            .relative_y = 0.1560043449675557,
+        },
+        0xf255...0xf256,
+        0xf25a,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9993997599039616,
+        },
+        0xf257,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.7810124049619848,
+            .relative_y = 0.0935945806894186,
+        },
+        0xf258,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.7498142113988452,
+            .relative_y = 0.1247927742525582,
+        },
+        0xf25b,
+        => .{
+            .size = .fit_cover1,
+            .height = .icon,
+            .align_horizontal = .center1,
+            .align_vertical = .center1,
+            .relative_height = 0.9975006099019084,
         },
         0xf296,
         => .{