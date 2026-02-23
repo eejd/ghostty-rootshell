ghostty-org/ghostty

renderer: added cursor style and visibility uniforms
Specifically:
iCurrentCursorStyle
iPreviousCursorStyle
iCurrentCursorVisible
iPreviousCursorVisible

Visibility calculated and updated independently from the typical cursor
unifrom updates to preserve cursor style even when not in the viewport
or set to be hidden

src/renderer/shaders/shadertoy_prefix.glsl
@@ -15,8 +15,18 @@ layout(binding = 1, std140) uniform Globals {
     uniform vec4  iPreviousCursor;
     uniform vec4  iCurrentCursorColor;
     uniform vec4  iPreviousCursorColor;
+    uniform int   iCurrentCursorStyle;
+    uniform int   iPreviousCursorStyle;
+    uniform int   iCurrentCursorVisible;
+    uniform int   iPreviousCursorVisible;
     uniform float iTimeCursorChange;
 };
+
+#define CURSORSTYLE_BLOCK        0
+#define CURSORSTYLE_BLOCK_HOLLOW 1
+#define CURSORSTYLE_BAR          2
+#define CURSORSTYLE_UNDERLINE    3
+#define CURSORSTYLE_LOCK         4
 
 layout(binding = 0) uniform sampler2D iChannel0;
 

eejd/ghostty-rootshell

src/renderer/shaders/shadertoy_prefix.glsl
@@ -15,6 +15,9 @@ layout(binding = 1, std140) uniform Globals {
     uniform vec4  iPreviousCursor;
     uniform vec4  iCurrentCursorColor;
     uniform vec4  iPreviousCursorColor;
+    uniform int   iCurrentCursorStyle;
+    uniform int   iPreviousCursorStyle;
+    uniform int   iCursorVisible;
     uniform float iTimeCursorChange;
     uniform float iTimeFocus;
     uniform int iFocus;
@@ -26,6 +29,12 @@ layout(binding = 1, std140) uniform Globals {
     uniform vec3  iSelectionForegroundColor;
     uniform vec3  iSelectionBackgroundColor;
 };
+
+#define CURSORSTYLE_BLOCK        0
+#define CURSORSTYLE_BLOCK_HOLLOW 1
+#define CURSORSTYLE_BAR          2
+#define CURSORSTYLE_UNDERLINE    3
+#define CURSORSTYLE_LOCK         4
 
 layout(binding = 0) uniform sampler2D iChannel0;
 